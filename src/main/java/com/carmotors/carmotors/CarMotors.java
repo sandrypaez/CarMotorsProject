5d6cdacc00893cdcff3c872a83946f42fbe32b60
YtzhakC <ytzhakcc@gmail.com>
2025-04-29 19:37:21 -0500

    Added Project Structure

---
+++ src/main/java/com/carmotors/carmotors/CarMotors.java
@@ -0,0 +1,8 @@
+package com.carmotors.carmotors;
+
+public class CarMotors {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/sta_carmotors/STA_CarMotors.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sta_carmotors;
+
+/**
+ *
+ * @author Ytzhak Dawidh
+ */
+public class STA_CarMotors {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
